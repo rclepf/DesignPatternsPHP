76d7ff6f7331d431f017f3fb718e2dd6d6afa74f
rclepf <rclepf@gmail.com>
2022-11-01 13:46:16 -0300

    execute

--- gera-pedido.php
+++ gera-pedido.php
@@ -2,20 +2,11 @@
 
 require_once 'vendor/autoload.php';
 
-use Projeto\DesignPatterns\{Orcamento, Pedido};
+use Projeto\DesignPatterns\{GerarPedido, Orcamento, Pedido};
 
 $valorOrcamento = $argv[1];
 $numeroDeItens = $argv[2];
 $nomeCliente = $argv[3];
 
-$orcamento = new Orcamento();
-$orcamento->quantidadeItens = $numeroDeItens;
-$orcamento->valor = $valorOrcamento;
-
-$pedido = new Pedido();
-$pedido->dataFinalizacao = new \DateTimeImmutable();
-$pedido->nomeCliente = $nomeCliente;
-$pedido->orcamento = $orcamento;
-
-echo "Cria pedido no banco de dados." . PHP_EOL;
-echo "Envia e-mail para cliente." . PHP_EOL;
+$gerarPedido = new GerarPedido($valorOrcamento, $numeroDeItens, $nomeCliente);
+$gerarPedido->execute();
